8f2c5013be8842ee0f191687c78746ba4d6b7195
kishore-das <vishal.turing@gmail.com>
2023-10-27 14:25:38 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="font-semibold">Hello !!!</h1>
+    </>
+  )
+}
+
+
